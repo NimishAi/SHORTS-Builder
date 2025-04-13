2810b0f03cce0c54195809559f4355322e9e0c8c
NimishChandola <nimish_chandola@yahoo.com>
2025-04-13 21:20:12 +0530

    stup code and styles

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning="true">
+    <html lang="en" suppressHydrationWarning="true" className="dark" style={{colorScheme:"dark"}}>
       <body
         className={`${outFit.className}`}
       >
